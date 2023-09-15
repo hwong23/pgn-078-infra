20f6d178ec59b154f10f4ae7c3cba91f8ff36a9a
hwo <hwong23@gmail.com>
2023-09-14 23:17:43 -0500

    archicli

---
+++ archicli.sh
@@ -0,0 +1,1 @@
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel /Users/hwo/gh/model/pgn-078-arq --script.runScript /Users/hwo/gh/jarchi-hwo/hwo/markdownDrivingViews-devdocsCLI.ajs -vistaDocumental $1
