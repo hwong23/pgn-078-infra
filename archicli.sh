a61992f3ad853b8fedd9cabd9e897d4066126481
hwo <hwong23@gmail.com>
2024-10-10 20:20:41 -0500

    upd

--- archicli.sh
+++ archicli.sh
@@ -1,1 +1,9 @@
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel /Users/hwo/gh/model/pgn-078-arq --script.runScript /Users/hwo/gh/jarchi-hwo/hwo/markdownDrivingViews-devdocsCLI.ajs -vistaDocumental $1
+# sh princiapl
+# Ejecución del programa para Extraer información del modelo de contenidos (archimate). 
+# Crear documento Markdown
+
+
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app \
+-consoleLog -nosplash --modelrepository.loadModel /Users/hwo/gh/model/pgn-078-arq \
+--script.runScript /Users/hwo/gh/jarchi-hwo/hwo/markdownDrivingViews-devdocsCLI.ajs \
+-vistaDocumental $1
